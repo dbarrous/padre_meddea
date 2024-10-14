53303b7d49a6b7a0a89331eb1665371fc36ff4b0
Damian Barrous-Dume <dbarrous@navteca.com>
2024-10-14 17:05:55 +0200

    Fix flake error after black linting

--- padre_meddea/__init__.py
+++ padre_meddea/__init__.py
@@ -13,11 +13,11 @@ except ImportError:
 SWXSOC_MISSION = os.getenv("SWXSOC_MISSION", "padre")
 os.environ["SWXSOC_MISSION"] = SWXSOC_MISSION
 
-from swxsoc import (
+from swxsoc import (  # noqa: E402
     config as swxsoc_config,
     log as swxsoc_log,
     print_config,
-)  # noqa: E402
+)
 
 # Load user configuration
 config = swxsoc_config
